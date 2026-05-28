16e839a18566dda4bab2555bc3a2ad68b4098983
ju4ncb <ju4ncb@gmail.com>
2026-05-28 04:32:21 -0500

    configuracion de env

--- app/core/database.py
+++ app/core/database.py
@@ -1,6 +1,6 @@
 from collections.abc import Generator
 
-from sqlalchemy import create_engine, inspect, text
+from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
 
 from app.core.config import settings
